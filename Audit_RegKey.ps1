ca0284d529a2d47ca46cd9ce482edde8afaac32a
NintendoWii <kyle.s.desjardins1@gmail.com>
2022-05-26 17:47:32 +0000

    Add files via upload

---
+++ Audit_RegKey.ps1
@@ -0,0 +1,24 @@
+﻿function Audit_RegKey{
+    Param(
+
+        [Parameter(Mandatory=$true)]
+    
+        [string]$Path
+
+        )
+
+    $valid= get-item -path $path -ErrorAction SilentlyContinue
+
+    if (!$valid){
+        Write-Host "[ERROR] Invalid Registry Path." -ForegroundColor Red
+    }
+
+    if ($valid){    
+        $RegKey_ACL = Get-Acl $path
+        $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Everyone","SetValue,CreateSubKey,Delete”,"none","none",”Success")
+        $RegKey_ACL.AddAuditRule($AccessRule)
+        $RegKey_ACL | Set-Acl $path
+        write-output "$path"
+        Write-Output "OK."
+    }
+}
