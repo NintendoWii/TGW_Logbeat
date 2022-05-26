78a5865f6751861c8022c7c5bcaed2e10135be85
NintendoWii <kyle.s.desjardins1@gmail.com>
2022-05-26 17:47:00 +0000

    Add files via upload

---
+++ Audit_RegKey.ps1
@@ -0,0 +1,10 @@
+﻿function Audit_RegKey($path){
+$valid= get-item -path $path
+    if ($valid){    
+        $RegKey_ACL = Get-Acl $path
+        $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Everyone","SetValue,CreateSubKey,Delete”,"none","none",”Success")
+        $RegKey_ACL.AddAuditRule($AccessRule)
+        $RegKey_ACL | Set-Acl $path
+        Write-Output "OK."
+    }
+}
